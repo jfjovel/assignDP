1164e4e0b2ced1b8d14a35371fcaff37b4aaf1a6
jfjovel <jjovel1@asu.edu>
2018-11-25 17:38:11 -0600

    Working on builder class with minor changes.

--- src/builder/Hive.java
+++ src/builder/Hive.java
@@ -1,5 +1,11 @@
 package builder;
 
+import decorator.Bee;
+
 public class Hive {
+    private String beeType = "";
+    private String beeAttribute = "";
+    private Bee queen;
+    private Bee[] workers;
 
 }
